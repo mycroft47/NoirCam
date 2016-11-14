bd062086eaa86b1c7aa32756b860f7b250f693a1
mycroft47 <youens@mac.com>
2016-11-14 02:44:38 -0800

    change bundle remote

--- NoirCam/ViewController.swift
+++ NoirCam/ViewController.swift
@@ -6,7 +6,7 @@
 //  Created by David McMahon on 6/28/15.
 //  Copyright (c) 2015 Bluewater Publishing. All rights reserved.
 //  modified sy 2016-05-22
-//   test for github 2016-11-14
+//  test for github 2016-11-14
 
 import UIKit
 import CoreImage
